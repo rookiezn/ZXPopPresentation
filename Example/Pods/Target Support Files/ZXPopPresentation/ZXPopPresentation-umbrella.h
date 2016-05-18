1a4b958c37ebf7ae0a3c4cb9afebc57ea1b0156f
rookiezn <rookiezn@gmail.com>
2016-05-18 11:16:09 +0800

    fix

--- Example/Pods/Target Support Files/ZXPopPresentation/ZXPopPresentation-umbrella.h
+++ Example/Pods/Target Support Files/ZXPopPresentation/ZXPopPresentation-umbrella.h
@@ -3,6 +3,8 @@
 #import "ZXPopPresentationController.h"
 #import "ZXPopViewController.h"
 #import "ZXTransitionAnimatorDefault.h"
+#import "ZXTransitionAnimatorFade.h"
+#import "ZXTransitionAnimatorFromBottom.h"
 
 FOUNDATION_EXPORT double ZXPopPresentationVersionNumber;
 FOUNDATION_EXPORT const unsigned char ZXPopPresentationVersionString[];
